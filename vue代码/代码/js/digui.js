ca5ce8dac7f7f793c733f6ce6a691fb36a85d7dd
Luominghong <1277391007@qq.com>
2019-07-31 15:48:29 +0800

    vue代码上传

---
+++ vue代码/代码/js/digui.js
@@ -0,0 +1,217 @@
+var data = [
+	{
+		"id": "1",
+		"data": {
+			"menuName": "part1",
+			"menuCode": "",
+		},
+		"childTreeNode": [
+			{
+				"data": {
+					"menuName": "HTML-CSS",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": [
+					{
+						"data": {
+							"menuName": "HTML部分",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": [{
+								"data": {
+									"menuName": "代码",
+									"menuCode": "BusProject",
+								},
+								"childTreeNode": []
+							},
+							{
+								"data": {
+									"menuName": "视频",
+									"menuCode": "BusProject",
+								},
+								"childTreeNode": []
+							}
+						]
+					},
+					{
+						"data": {
+							"menuName": "CSS部分",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": [{
+							"data": {
+								"menuName": "代码",
+								"menuCode": "BusProject",
+							},
+							"childTreeNode": []
+						}]
+					}
+				]
+			}, {
+				"data": {
+					"menuName": "Javascript初级",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": [{
+					"data": {
+						"menuName": "变量",
+						"menuCode": "BusProject",
+					},
+					"childTreeNode": []
+				}, {
+					"data": {
+						"menuName": "运算符",
+						"menuCode": "BusProject",
+					},
+					"childTreeNode": []
+				}, {
+					"data": {
+						"menuName": "流程表达式",
+						"menuCode": "BusProject",
+					},
+					"childTreeNode": []
+				}, ]
+			}, {
+				"data": {
+					"menuName": "Javascript中级",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			},
+			{
+				"data": {
+					"menuName": "Javascript高级",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			},
+			{
+				"data": {
+					"menuName": "Jquery",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			},
+			{
+				"data": {
+					"menuName": "HTML5",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			}, {
+				"data": {
+					"menuName": "CSS3",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			}, {
+				"data": {
+					"menuName": "ES6",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			},
+		]
+	},
+	{
+		"id": "2",
+		"data": {
+			"menuName": "part2",
+			"menuCode": "",
+		},
+		"childTreeNode": [{
+				"data": {
+					"menuName": "linux和git命令",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			},
+			{
+				"data": {
+					"menuName": "artTemplate模板引擎",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": [{
+					"data": {
+						"menuName": "代码",
+						"menuCode": "BusProject",
+					},
+					"childTreeNode": [{
+						"data": {
+							"menuName": "basisRender",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": []
+					}, {
+						"data": {
+							"menuName": "条件渲染",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": []
+					}, {
+						"data": {
+							"menuName": "循环渲染",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": []
+					}, {
+						"data": {
+							"menuName": "过滤器",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": []
+					}, {
+						"data": {
+							"menuName": "模板复用",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": []
+					}, {
+						"data": {
+							"menuName": "外部模板引用",
+							"menuCode": "BusProject",
+						},
+						"childTreeNode": []
+					}, ]
+				}, {
+					"data": {
+						"menuName": "视频",
+						"menuCode": "BusProject",
+					},
+					"childTreeNode": []
+				}, {
+					"data": {
+						"menuName": "课件",
+						"menuCode": "BusProject",
+					},
+					"childTreeNode": []
+				}, ]
+			},
+			{
+				"data": {
+					"menuName": "webpack",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			}, {
+				"data": {
+					"menuName": "sass",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			}, {
+				"data": {
+					"menuName": "echart",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			}, {
+				"data": {
+					"menuName": "vue",
+					"menuCode": "BusProject",
+				},
+				"childTreeNode": []
+			},
+		]
+	}
+]
